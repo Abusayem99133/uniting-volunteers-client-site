a1c74e9d98121c3b22f5a80e53b55d489f350db1
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-10 02:15:39 +0600

    register page firbase and ui work done

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 import login from "../../assets/image/login-img.jpg";
 import { Helmet } from "react-helmet-async";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import toast, { Toaster } from "react-hot-toast";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 const Register = () => {
   const { createUser } = useContext(AuthContext);
+  const [showPassword, setShowPassword] = useState(false);
   const handleRegister = (event) => {
     event.preventDefault();
     const form = new FormData(event.currentTarget);
@@ -13,6 +17,14 @@ const Register = () => {
     const email = form.get("email");
     const password = form.get("password");
     console.log(name, photo, email, password);
+    if (password.length < 6) {
+      toast.error("Password should be at least 6 character.!");
+      return;
+    } else if (!/(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(password)) {
+      toast.error("please show the on character in Uppercase and Lowercase");
+    } else {
+      toast.success("Successfully Register");
+    }
     createUser(email, password)
       .then((result) => {
         const user = result.user;
@@ -25,7 +37,7 @@ const Register = () => {
       <Helmet>
         <title>Register-page</title>
       </Helmet>
-      <div className="flex w-full p-5 bg-blue-600">
+      <div className="flex w-full p-5 bg-blue-600 items-center">
         <div className="w-1/2">
           <form onSubmit={handleRegister} className="card-body">
             <h1 className="text-4xl text-white text-center shadow-sm">
@@ -69,28 +81,29 @@ const Register = () => {
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Password</span>
+                <span className="label-text text-white">Password</span>
               </label>
               <input
-                type="password"
-                placeholder="password"
+                type={showPassword ? "text" : "password"}
                 name="password"
+                placeholder="password"
                 className="input input-bordered"
                 required
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className="ml-96 -mt-8"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Register</button>
             </div>
             <h2>
-              Ready For Login Now{" "}
-              <Link className="text-white " to="/login">
-                ---Login
+              Ready For Login Now..!{" "}
+              <Link className="text-white text-center font-bold" to="/login">
+                Login
               </Link>
             </h2>
             <hr />
@@ -101,6 +114,7 @@ const Register = () => {
           <img src={login} alt="" />
         </div>
       </div>
+      <Toaster />
     </div>
   );
 };
