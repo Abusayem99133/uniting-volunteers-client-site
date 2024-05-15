6f075b79da3fe366cd41a518dfe878fc6caef41c
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-15 18:46:34 +0600

    error page work done

--- src/Shared/ErrorPage.jsx
+++ src/Shared/ErrorPage.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h1 className="text-4xl text-orange-600">This is Error Page</h1>
+    <div className="mx-auto max-h-screen flex-auto  items-center text-center">
+      <h1 className="text-6xl text-orange-600 font-bold ">Oops!</h1>
+      <h3 className="mt-4">404- PAGE NOT FOUND</h3>
+      <p>
+        The page you are looking for might have been removed had its name or is
+        temporarily unavailable.
+      </p>
+      <Link to="/">
+        <button className="btn bg-blue-400">Go To Homepage</button>
+      </Link>
     </div>
   );
 };
